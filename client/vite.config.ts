ae6fa129c759e2741e388192e54cb03ba83715ca
Anastasiya <anastasiya.ibr@mail.ru>
2025-01-06 16:35:35 +0300

    feat: подготовка к pwa

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,39 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3003", // адрес Вашего сервера
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""), // если нужно изменить путь
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      outDir: "dist",
+      manifest: {
+        name: "Motes-App",
+        short_name: "NotesApp",
+        description: "Notes App",
+        theme_color: "#f9f0ff",
+        start_url: "/",
+        display: "standalone",
+        icons: [
+          {
+            src: "./icons/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./icons/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "./icons/favicon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
       },
-    },
-  },
+    }),
+  ],
 });
