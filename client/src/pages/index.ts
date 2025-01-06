d759d79215a65badcb9e9490fa29079b8536c912
Anastasiya <anastasiya.ibr@mail.ru>
2025-01-06 11:57:20 +0300

    feat: добавлена страница с инфо

--- client/src/pages/index.ts
+++ client/src/pages/index.ts
@@ -2,3 +2,4 @@ export * from "./homePage/HomePage.tsx";
 export * from "./signUpPage/SignUpPage.tsx";
 export * from "./signInPage/SignInPage.tsx";
 export * from "./notePage/NotePage.jsx";
+export * from "./aboutPage/AboutPage.tsx";
